02714701abc697e4c0063e114e261df24ab9188d
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2020-05-31 21:17:40 +0300

    Add posts

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,18 @@ Route::get('/', function () {
 Route::get('/padawan', function () {
 	return view('padawan');
 });
+
+Route::get('/posts/{post}', function ($post) {
+	$posts = [
+		'my-first-post' => 'Hello, this is my first post dawg!',
+		'my-second-post' => 'Yo,  this is my second post.'
+	];
+
+	if (! array_key_exists($post, $posts)) {
+		abort(404, 'Sorry, not found');
+	}
+
+	return view('post', [
+		'post' => $posts[$post]
+	]);
+});
