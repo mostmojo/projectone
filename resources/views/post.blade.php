9c199bdaf3bceb28a10ce72cf8d52e17e8356f36
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2020-05-31 21:39:49 +0300

    Add styles to blogs

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,10 +4,23 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>My blog</title>
+ <!-- Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+	<!-- Styles -->
+	<style>
+			html, body {
+					background-color: #fff;
+					color: #636b6f;
+					font-family: 'Nunito', sans-serif;
+					font-weight: 200;
+					height: 100vh;
+			}
+	</style>
 </head>
 <body>
-<h1>My blog</h1>
-<p>{{ $post }}</p>
-<a href="/">Home</a>
+	<h1>My blog</h1>
+	<p>{{ $post }}</p>
+	<a href="/">Home</a>
 </body>
 </html>
