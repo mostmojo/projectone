49c10ad90471660b4368a5658ffcab55fbf846c6
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2020-05-31 21:34:41 +0300

    Add controller

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>My blog</title>
+</head>
+<body>
 <h1>My blog</h1>
-
 <p>{{ $post }}</p>
+<a href="/">Home</a>
+</body>
+</html>
